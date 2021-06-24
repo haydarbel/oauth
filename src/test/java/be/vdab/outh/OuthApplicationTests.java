2a362b88eb8768b04030872ff0fa2e2fb4153fae
haydar koktas <haydarkoktas@gmail.com>
2021-06-24 15:05:25 +0200

    de eerste commit

---
+++ src/test/java/be/vdab/outh/OuthApplicationTests.java
@@ -0,0 +1,13 @@
+package be.vdab.outh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OuthApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
